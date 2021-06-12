bd8bfb60dde83901df94e30be7dce12c58200041
Shubham.16 <shubhamlakade1994@gmail.com>
2021-06-12 18:56:09 +0530

    New file added

---
+++ Separate.java
@@ -0,0 +1,37 @@
+package All;
+
+import java.util.Scanner;
+
+public class Separate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int arr[] = { 1,0,0,1,0 }; 
+
+		//initialize variable
+		int count = 0;
+
+		// add for loop
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 0) {
+				count++;
+			}
+
+		}
+		
+		for (int i = 0; i < count; i++) {
+			arr[i] = 0;
+		}
+		
+		for (int i = count; i < arr.length; i++) {
+			arr[i] = 1;
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
